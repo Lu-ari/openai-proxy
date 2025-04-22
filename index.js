dc178114a52c714e2b46cc236e8ae30177c25d66
Mehmet Akif Özdemir <89011948+Lu-ari@users.noreply.github.com>
2025-04-22 05:11:09 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  const targetUrl = req.url.slice(1);
+
+  if (!targetUrl.startsWith("http")) {
+    return res.status(400).send("Lütfen geçerli bir URL girin.");
+  }
+
+  const proxy = createProxyMiddleware({
+    target: targetUrl,
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+      "^/": "",
+    },
+    onProxyReq: (proxyReq) => {
+      proxyReq.setHeader("Origin", targetUrl);
+    },
+  });
+
+  return proxy(req, res, next);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(Proxy sunucusu ${port} portunda çalışıyor.);
+});
